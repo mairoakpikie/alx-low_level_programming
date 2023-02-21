1b5d979603ba4b4feb5568dea5daf2f4217c22d8
Mairo <mairoakpikie@gmail.com>
2023-02-21 14:02:01 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - To print out 10 single numbers
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('%d', i);
+}
+putchar('\n');
+return (0);
+}
